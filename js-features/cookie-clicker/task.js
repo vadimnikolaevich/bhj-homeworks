727e5a0e84acd7b2c6b674301b0bd187bba23365
vadimnikolaevich <vadimlitvincuk@gmail.com>
2023-03-10 16:42:42 +0300

    clickerSpeed

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,11 +1,20 @@
-countdown();
-function countdown() {
-    seconds = (document.getElementById('timer').textContent)--;
-  
-    if (seconds <= 0) {
-      clearTimeout(timer);
-      alert('Вы победили в конкурсе!')
-    } else {
-        setTimeout(countdown, 1000);
-    }
+let clickerSpeed = document.getElementById('clicker__speed');
+let timeStart= new Date();
+
+function cookieCliker() {
+    click = (document.getElementById('clicker__counter').textContent)++
+
+    image.width = click++ % 2 ? 200 : 170;
+    // if (click % 2) {
+    //     image.width = 200;
+    // } else {
+    //     image.width = 170;
+    // }
+    
+    let timeNow = new Date();
+    clickerSpeed.textContent = (1 / ((timeNow.getTime() - timeStart.getTime()) / 1000)).toFixed(2);
+    timeStart = new Date();
 }
+
+let image = document.getElementById('cookie');
+image.onclick = cookieCliker;
