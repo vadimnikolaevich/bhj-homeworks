6b13ff730343e14f81c2e3c3ac7cee19538e1c99
vadimnikolaevich <vadimlitvincuk@gmail.com>
2023-03-15 00:20:13 +0300

    2

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,17 +1,18 @@
-const modalMain = document.querySelector('#modal_main');
-const modalSuccess = document.querySelector('#modal_success');
-const showSuccess = document.querySelector('.show-success');
-const cross = document.querySelectorAll('.modal__close');
+const mainMod = document.getElementById('modal_main');
+const successMod = document.getElementById('modal_success');
+const successShow = document.querySelector('.show-success');
+const closes = document.querySelectorAll('div.modal__close');
 
-modalMain.classList.toggle('modal_active');
+mainMod.classList.toggle('modal_active');
+successShow.onclick = () => {
+    mainMod.classList.toggle('modal_active');
+    successMod.classList.toggle('modal_active');
+}
 
-Array.from(cross).forEach((btn) => {
-    btn.onclick = () => {
-        btn.closest('.modal').classList.remove('modal_active');
+for (let i = 0; i < closes.length; i++) {
+    let close = closes[i];
+    close.onclick = () => {
+        mainMod.classList.remove('modal_active');
+        successMod.classList.remove('modal_active');
     }
-});
-
-showSuccess.onclick = () => {
-    modalMain.classList.remove('modal_active');
-    modalSuccess.classList.add("modal_active");
 }
