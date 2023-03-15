b66d2a72fe449b9b4c9935de84ee915330f8a686
vadimnikolaevich <vadimlitvincuk@gmail.com>
2023-03-15 13:59:28 +0300

    очередное исправление

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,39 +1,28 @@
-const slides = Array.from(document.getElementsByClassName('slider__item'));
-const dots = Array.from(document.getElementsByClassName('slider__dot'));
-const arrowNext = document.querySelector('.slider__arrow_next');
+
+const divImgs = document.querySelectorAll('.slider__item');
 const arrowPrev = document.querySelector('.slider__arrow_prev');
-let index = 0;
+const arrowNext = document.querySelector('.slider__arrow_next');
+const arrayImg = Array.from(divImgs);
 
-function changeSlides (index) {
-    slides[index].classList.toggle('slider__item_active');
-    dots[index].classList.toggle('slider__dot_active');
-}
-
-arrowNext.onclick = () => {
-    changeSlides(index);
-    index = index < slides.length - 1 ? index + 1 : 0;
-    changeSlides(index);
-}
-
-arrowPrev.onclick = () => {
-    changeSlides(index);
-    index = index > 0 ? index - 1 : slides.length - 1;
-    changeSlides(index);
-}
-
-function dotsToggle(index) {
-    const activeDotIndex = dots.findIndex(dot => dot.classList.contains('slider__dot_active'));
-    dots[activeDotIndex].classList.toggle('slider__dot_active');
-    slides[activeDotIndex].classList.toggle('slider__item_active');
-
-    dots[index].classList.toggle('slider__dot_active');
-    slides[index].classList.toggle('slider__item_active');
-}
-
-for (let i = 0; i < dots.length; i++) {
-    dots[i].addEventListener('click', () => {
-        dotsToggle(index);
-        index = i;
-        dotsToggle(index);
-    })
-}
+function leafNext(){
+    let indexActive = arrayImg.findIndex(element => element.classList.contains("slider__item_active"));
+    arrayImg[indexActive].classList.remove('slider__item_active');
+    if(indexActive < (arrayImg.length-1)){
+        indexActive += 1;
+    } else{
+        indexActive = 0;
+    };
+    return arrayImg[indexActive].classList.add('slider__item_active');
+};
+function leafPrev(){
+    let indexActive = arrayImg.findIndex(element => element.classList.contains("slider__item_active"));
+    arrayImg[indexActive].classList.remove('slider__item_active');
+    if(indexActive > 0){
+        indexActive -= 1;
+    } else{
+        indexActive = (arrayImg.length-1);
+    };
+    return arrayImg[indexActive].classList.add('slider__item_active');
+};
+arrowNext.addEventListener('click', leafNext);
+arrowPrev.addEventListener('click', leafPrev);
