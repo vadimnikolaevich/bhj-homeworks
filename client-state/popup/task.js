30f556858d4dc5f0f3d8ebdf0681b0cc9beb49c0
vadimnikolaevich <vadimlitvincuk@gmail.com>
2023-04-13 22:52:48 +0300

    1

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,24 +1,17 @@
-'use strict';
-// задание переменных 
-const modal = document.querySelector('.modal');
+const modal = document.getElementById('subscribe-modal');
 const modalClose = document.querySelector('.modal__close');
-// задание cookie
-if(document.cookie !== 'modal_active=on' && document.cookie !== 'modal_active=off'){
-    document.cookie = 'modal_active=on; max-age=3600; secure';
-    modal.classList.add('modal_active');
+
+const pairs = document.cookie.split('; ')
+const cookie = pairs.find((i) => i.startsWith('modal' + '='));
+
+if (!cookie) {
+  modal.classList.add('modal_active');
+} else {
+  modal.classList.remove('modal_active');
 };
-const lengthCookie = 'modal_active='.length;
-const cookie = document.cookie.slice(lengthCookie, document.cookie.length);
-if(cookie === 'off'){
-    modal.classList.remove('modal_active');
-} else{
-    modal.classList.add('modal_active');
-};
-console.log(cookie);
-console.log(document.cookie);
-// задаем обработчик событий
-modalClose.addEventListener('click', function(){
-    modal.classList.remove('modal_active');
-    document.cookie = 'modal_active=off; max-age=3600; secure';
-    console.log(document.cookie);
+
+modalClose.addEventListener('click', (event) => {
+  modal.classList.remove('modal_active');
+  document.cookie = 'modal=close';
+  event.preventDefault();
 });
